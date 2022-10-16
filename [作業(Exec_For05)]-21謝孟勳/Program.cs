63fec08d5da02c78f9d4b40f63203fcfedf7ed9a
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:17:33 +0800

    加入專案檔案。

---
+++ [作業(Exec_For05)]-21謝孟勳/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_For05___21謝孟勳
+{
+    //用迴圈呈現指定右側的結果
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string row = string.Empty;
+            
+            for (int i = 0; i <= 5; i++)
+            { 
+                int countOfint = 4-i;
+                int countOfnumber = i;
+                
+                string row = new string('+' , countOfint) + new string('i', countOfnumber);
+                Console.WriteLine(row);
+
+            }
+
+        }
+    }
+}
